e7efefcc9cebb84b3c948c24e5400bc8356154c6
Tomok2404 <tomok2404@web.de>
2026-04-19 20:11:09 +0200

    Initial Plugin Setup
    Renaming, change author, etc.

--- FFFeudPlugin/Configuration.cs
+++ FFFeudPlugin/Configuration.cs
@@ -1,7 +1,7 @@
-﻿using Dalamud.Configuration;
+using Dalamud.Configuration;
 using System;
 
-namespace SamplePlugin;
+namespace FFFeudPlugin;
 
 [Serializable]
 public class Configuration : IPluginConfiguration
